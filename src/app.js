0b5d13d6b96ec9f4470b2eb5220fded22df4be8b
Charlie Sparks <csparks@antioch.edu>
2018-03-09 12:21:08 -0800

    yesterday's value is working, need to combine into a single request

--- src/app.js
+++ src/app.js
@@ -10,24 +10,36 @@ const port = 3000;
 //middleware setup
 server.use(bodyParser.json());
 
+//initializing variables to hold the coin rates
+let currentVal, previousVal, changeInVal;
+
 //initial get handler for requests to the "/" URL
 server.get("/", (req, res) => {
     res.send("<h1>Litcoin!</h1>");
 })
 
+
+
 //get handler and fetch for today's values
+// server.get("/compare", (req, res) => {
+//     fetch("https://api.coindesk.com/v1/bpi/currentprice/XBT.json")
+//     .then(response => response.json())
+//     .then(data => {
+//         currentVal = data.bpi.USD.rate;
+//         res.send(`Today's value is: $${currentVal} USD per Bitcoin`);
+//     });
+// })
+
+//get handler and fetch for tomorrow's values
 server.get("/compare", (req, res) => {
-    let currentVal;
-    fetch("https://api.coindesk.com/v1/bpi/currentprice/XBT.json")
+    fetch("https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday")
     .then(response => response.json())
     .then(data => {
-        currentVal = data.bpi.USD.rate;
-        res.send(`Today's value is: $${currentVal} USD per Bitcoin`);
-    });
+        let value = Object.values(data.bpi);
+        previousVal = value;
+        res.send(`Yestday's value was: $${previousVal} USD per Bitcoin`);
+    })
 })
-
-
-
 
 
 
